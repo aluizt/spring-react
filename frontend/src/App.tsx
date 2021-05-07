22bd38ffb8ab070fbf5dcd3ca5d498dd702603e5
alexandre <altavares@grupodimed.com.br>
2021-05-06 22:33:53 -0300

    Static charts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,8 @@
+import BarChart from 'components/BarChart/barchart'
+import DonutChart from 'components/DonutChart/donutchart'
 import Footer from 'components/Footer/footer'
 import DataTable from 'components/TadaTable/tadatable'
+import React from 'react'
 import NavBar from '../src/components/NavBar/navbar'
 
 function App() {
@@ -7,7 +10,20 @@ function App() {
     <>
       <NavBar />
       <div className="container">
-        <h1 className="text-primary">Hellou World</h1>
+        <h1 className="text-primary py3">Dashboard de Vendas</h1>
+        <div className="row px-3">
+          <div className="colsm-6">
+            <h5 className="text-center text-secondary">Taxa de sucesso (%)</h5>
+            <BarChart/>
+          </div>
+          <div className="colsm-6">
+            <h5 className="text-center text-secondary">Taxa de sucesso</h5>
+            <DonutChart/>
+          </div>
+        </div>
+        <div className="py-3">
+          <h2 className="text-primary">Totas vendas</h2>
+        </div>
         <DataTable/>
       </div>
       <Footer/>
